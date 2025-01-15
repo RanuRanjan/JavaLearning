40a048058c91916d84e9743075a61a8c488ef781
Ranu Ranjan <ranuranjan25@gmail.com>
2025-01-15 18:53:19 +0530

    Scanner taking input

---
+++ src/main/java/org/example/TakingInput.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class TakingInput {
+
+    public static void main(String[] args) {
+
+
+        Scanner username = new Scanner(System.in);
+        String usernames = username.nextLine();
+        System.out.println("userDetail");
+        System.out.println(usernames);
+
+
+    }
+
+}
