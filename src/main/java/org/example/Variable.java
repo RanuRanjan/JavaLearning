db126055741ddeac031d7d98a18123498f807d6e
Ranu Ranjan <ranuranjan25@gmail.com>
2025-01-15 14:08:59 +0530

    Variable

---
+++ src/main/java/org/example/Variable.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Variable {
+    public static void main(String[] args) {
+
+        String employeeName = "Ranu";
+        int age = 20;
+        char gender = 'M';
+        boolean govtEmplyoee = false;
+    }
+}
